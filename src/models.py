Fixed ordering of normalisation and pooling

src/models.py
@@ -1,14 +1,13 @@
-import inspect
 import math
 from dataclasses import dataclass
 from enum import Enum
-from typing import List
+from typing import List, Optional
 
 from torch.nn import ReLU, Linear, Sequential
 from torch_geometric.nn import (
     GCNConv, GINConv, GATConv, GATv2Conv,
     global_mean_pool, global_max_pool, global_add_pool,
-    Sequential as SequentialGNN
+    Sequential as SequentialGNN, BatchNorm
 )
 
 
@@ -26,7 +25,7 @@ class HyperParameters:
     max_epochs: int
 
 
-class Layer(Enum):
+class LayerType(Enum):
     GCN = GCNConv
     GIN = GINConv
     GAT = GATConv
@@ -46,9 +45,10 @@ class PoolingFunction(Enum):
 
 @dataclass
 class ModelArchitecture:
-    layer_types: List[Layer]
+    layer_types: List[LayerType]
     features: List[int]
-    activation_funcs: List[ActivationFunction]
+    activation_funcs: List[Optional[ActivationFunction]]
+    batch_normalise: List[bool]
 
     def __str__(self):
         return f"ModelArchitecture({self._base_inner_str()})"
@@ -80,18 +80,23 @@ class GNNArchitecture(ModelArchitecture):
 
 def construct_gnn(arch: GNNArchitecture) -> SequentialGNN:
     global_inputs = "x, edge_index, batch"
+    num_layers = len(arch.layer_types)
     layers = []
-    for layer_type, num_in, num_out, activation in zip(
-        arch.layer_types,
-        arch.features[:-1],
-        arch.features[1:],
-        arch.activation_funcs
-    ):
+    for i in range(num_layers):
+        layer_type = arch.layer_types[i]
+        num_in = arch.features[i]
+        num_out = arch.features[i + 1]
+        activation = arch.activation_funcs[i]
+        normalise = arch.batch_normalise[i]
         layer = _construct_layer(layer_type, num_in, num_out)
         layers.append((layer, "x, edge_index -> x"))
+        if i == num_layers - 1:
+            layers.append((arch.pool_func, "x, batch -> x"))
+        if normalise:
+            layers.append(BatchNorm(num_out))
         if activation is not None:
             layers.append(activation.value(inplace=True))
-    layers.append((arch.pool_func, "x, batch -> x"))
+
     return SequentialGNN(global_inputs, layers)
 
 
@@ -112,7 +117,7 @@ def construct_mlp(arch: ModelArchitecture) -> Sequential:
 
 def _construct_layer(layer_type, num_in, num_out):
     match layer_type:
-        case Layer.GIN:
+        case LayerType.GIN:
             # TODO: Add customisable layer architectures
             num_hidden = int(math.sqrt(num_in + num_out))
             mlp = Sequential(Linear(num_in, num_hidden), ReLU(), Linear(16, num_hidden))