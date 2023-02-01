4dbe07ec6a8545b0e792b6e324a5d68386234965
harryknighton <mrmillipede01@gmail.com>
2023-02-01 17:22:49 +0000

    Copied MF-PCBA Seeds

--- src/config.py
+++ src/config.py
@@ -20,7 +20,7 @@ DEFAULT_PARAMETERS = HyperParameters(
     use_sd_readouts=False,
     k_folds=6,
     test_split=0.2,
-    train_val_split=0.8,
+    train_val_split=0.75,
     batch_size=32,
     early_stop_patience=10,
     early_stop_min_delta=0.01,
@@ -35,3 +35,11 @@ DEFAULT_METRICS = MetricCollection([
     PearsonCorrCoefSquared(),
     R2Score()
 ])
+
+# Seeds used in https://chemrxiv.org/engage/chemrxiv/article-details/636fa49b80c9bfb4dc944c1c
+# From https://github.com/davidbuterez/mf-pcba
+RANDOM_SEEDS = {
+    "AID1445": [946067, 721263, 691383, 374914, 724299],
+    "AID504329": [966204, 681725, 635271, 220018, 548422],
+    "AID624330": [693665, 109746, 780835, 662995, 865845]
+}
