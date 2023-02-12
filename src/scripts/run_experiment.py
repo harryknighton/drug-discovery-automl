eda756bd455c7321211731f3bb4db1c875634085
harryknighton <mrmillipede01@gmail.com>
2023-02-12 11:02:20 +0000

    Added argparse commands

--- src/scripts/run_experiment.py
+++ src/scripts/run_experiment.py
@@ -1,21 +1,35 @@
+import argparse
+
+from src.config import RANDOM_SEEDS
 from src.data import DatasetUsage
 from src.models import build_uniform_gnn_architecture, GNNLayerType, PoolingFunction, ActivationFunction, \
     HyperParameters
 from src.training import run_experiment
 
-if __name__ == '__main__':
+
+def main():
+    parser = argparse.ArgumentParser(description='Run Experiment')
+    parser.add_argument('--name', type=str, required=True)
+    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--epochs', type=int, required=False, default=100)
+    parser.add_argument('--use-mf-pcba-splits', action=argparse.BooleanOptionalAction, default=False)
+
+    args = vars(parser.parse_args())
+    _validate_args(args)
+
     params = HyperParameters(
-        random_seed=0,
+        random_seed=1424,
         use_sd_readouts=False,
         k_folds=1,
         test_split=0.2,
-        train_val_split=0.75,
+        train_val_split=0.8,
         batch_size=32,
         early_stop_patience=30,
         early_stop_min_delta=0.01,
         lr=0.0001,
-        max_epochs=5
+        max_epochs=args['epochs']
     )
+
     architectures = []
     for layer_type in GNNLayerType:
         architectures.append(
@@ -26,8 +40,17 @@ if __name__ == '__main__':
                 pool_func=PoolingFunction.ADD,
                 batch_normalise=True,
                 activation=ActivationFunction.ReLU,
-                regression_layers=2
+                num_regression_layers=2
             )
         )
 
-    run_experiment('uniform_test', 'AID1445', DatasetUsage.DROnly, architectures, params, [1424])
+    run_experiment(args['name'], args['dataset'], DatasetUsage.DROnly, architectures, params, [1424])
+
+
+def _validate_args(args: dict):
+    assert args['dataset'] in RANDOM_SEEDS.keys()
+    assert args['epochs'] > 0
+
+
+if __name__ == '__main__':
+    main()
