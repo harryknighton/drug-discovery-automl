d5e2f2a182e131ceefe0a3824bf39e7b977ea6fd
harryknighton <mrmillipede01@gmail.com>
2023-02-25 12:49:01 +0000

    Changed NAS results saving

--- src/nas.py
+++ src/nas.py
@@ -1,17 +1,17 @@
-import json
+import pickle
 from pathlib import Path
 
 import hyperopt
 from hyperopt import hp
 from torch_geometric.data import LightningDataset
 
-from src.config import LOG_DIR, DEFAULT_N_FEATURES, DEFAULT_LOGGER, RANDOM_SEEDS
+from src.config import LOG_DIR, DEFAULT_N_FEATURES, DEFAULT_LOGGER
 from src.data import HTSDataset, partition_dataset
 from src.models import PoolingFunction, GNNLayerType, ActivationFunction, GNNArchitecture, \
     build_uniform_regression_layer_architecture
-from src.parameters import DatasetUsage, HyperParameters, BasicSplit, MFPCBA
+from src.parameters import DatasetUsage, HyperParameters, BasicSplit
 from src.reporting import generate_experiment_dir
-from src.training import train_model, perform_run
+from src.training import train_model
 
 
 def search_hyperparameters(
@@ -48,21 +48,7 @@ def search_hyperparameters(
         trials=trials
     )
     best_architecture = _convert_to_gnn_architecture(hyperopt.space_eval(search_space, best))
-    test_params = HyperParameters(
-        random_seed=seed,
-        dataset_usage=dataset_usage,
-        dataset_split=MFPCBA(RANDOM_SEEDS[dataset_name]),
-        batch_size=32,
-        early_stop_patience=30,
-        early_stop_min_delta=0,
-        lr=3e-5,
-        max_epochs=100,
-        num_workers=num_workers,
-        limit_batches=1.0
-    )
-    best_results = perform_run(dataset, best_architecture, test_params, experiment_dir, run_name='best')
     DEFAULT_LOGGER.info(f"Best architecture: {best_architecture}")
-    DEFAULT_LOGGER.info(f"Best performance: {best_results}")
     _save_trials(trials, experiment_dir)
 
 
@@ -124,6 +110,5 @@ def _convert_to_gnn_architecture(space):
 
 
 def _save_trials(trials: hyperopt.Trials, experiment_dir: Path) -> None:
-    losses = trials.losses()
-    with open(experiment_dir / 'losses.json', 'w') as file:
-        json.dump(losses, file)
+    with open(experiment_dir / 'trials.pkl', 'w') as out:
+        pickle.dump(trials, out)
