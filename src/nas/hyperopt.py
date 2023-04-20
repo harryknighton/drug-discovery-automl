Fixed proxy calculation on GPU

src/nas/hyperopt.py
@@ -145,6 +145,11 @@ def _prepare_objective(
 ) -> Callable[[Any], Dict[str, Any]]:
     tl.seed_everything(params.random_seeds[0], workers=True)
     assert isinstance(params.dataset_split, BasicSplit)
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    dataset.dataset.data = dataset.dataset.data.to(device)
+    dataset.label_scaler = dataset.label_scaler.to(device)
+
     test_dataset, training_dataset = split_dataset(dataset.dataset, params.dataset_split.test_split)
     train_dataset, val_dataset = split_dataset(training_dataset, params.dataset_split.train_val_split)
     datamodule = LightningDataset(
@@ -155,7 +160,7 @@ def _prepare_objective(
 
     def objective(x):
         architecture = _convert_to_gnn_architecture(x, dataset.dataset.num_features, dataset.dataset.num_classes)
-        model = GNN(architecture)
+        model = GNN(architecture).to(device)
         AUTOML_LOGGER.debug("Evaluating architecture " + str(architecture))
         metrics = {}
         status = STATUS_OK

src/nas/proxies.py
@@ -64,8 +64,8 @@ class Ensemble(Proxy):
     def _compute(self, model: GNNModule, dataset: NamedLabelledDataset) -> Tensor:
         proxies = self.proxy_collection(model, dataset)
         x = torch.stack(list(proxies.values())).reshape(1, -1)
-        result = self.model.predict(x)
-        return torch.tensor(result)
+        result = self.model.predict(x.cpu())
+        return torch.tensor(result, device=x.device)
 
     def fit(self, xs: Metrics, y: Tensor, minimise_y: bool) -> None:
         self.proxy_collection.fit(xs, y, minimise_y)
@@ -80,16 +80,19 @@ class Ensemble(Proxy):
 
 class NumParams(Proxy):
     def _compute(self, model: GNNModule, dataset: NamedLabelledDataset) -> Tensor:
-        return torch.tensor(sum(p.numel() for p in model.parameters() if p.requires_grad), dtype=torch.float)
+        return torch.tensor(
+            sum(p.numel() for p in model.parameters() if p.requires_grad),
+            dtype=torch.float, device=next(model.parameters()).device
+        )
 
 
 class SynFlow(Proxy):
     def _compute(self, model: GNNModule, dataset: NamedLabelledDataset) -> Tensor:
         weights = _get_model_weights(model)
         preds = model(
-            x=torch.ones(1, dataset.dataset.num_features, dtype=torch.float),
-            edge_index=torch.tensor([[0], [0]], dtype=torch.long),
-            batch=torch.tensor([0], dtype=torch.long)
+            x=torch.ones(1, dataset.dataset.num_features, dtype=torch.float, device=weights[0].device),
+            edge_index=torch.tensor([[0], [0]], dtype=torch.long, device=weights[0].device),
+            batch=torch.tensor([0], dtype=torch.long, device=weights[0].device)
         )
         loss = preds.sum()
         first_derivatives = autograd.grad(loss, weights, allow_unused=True)